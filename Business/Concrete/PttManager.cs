9a9de3da37e5ee6a3ddc1bc9dfa771b80e271623
OrhanScript <ezalorhan040@gmail.com>
2024-04-05 20:56:21 +0300

    first commit

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+        
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine("Mask was given to " + person.FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Mask was not given to " + person.FirstName);
+            }
+           
+            
+        }
+    }
+}
---
+++ Proje3 denemeler/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SelamVer();
+        SelamVer();
+        SelamVer();
+        SelamVer();
+
+        PttManager pttManager = new PttManager(new ApplicantManager());
+
+        Console.ReadLine();
+    }
+    static void SelamVer()
+    {
+        Console.WriteLine("Merhaba");
+    }
+}
